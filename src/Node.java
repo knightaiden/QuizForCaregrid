756dfbc96a34c98acb951c206a94ee969dcd1cf7
Aiden Zhang <aiden_zhang2012@163.com>
2019-10-27 22:14:00 +1100

    added node

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zhangzhe on 27/10/19.
+ *
+ * Feel free to use this structure, if you have your own design, pls just ignore it.
+ */
+public class Node {
+
+    public int value;
+    public Node next;
+
+}
